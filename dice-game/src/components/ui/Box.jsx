a198e3a3fe8194cd4903055e9e10bbf6c98508a2
Shivanshu <ks7876555@gmail.com>
2024-01-22 21:25:47 +0530

    minor changes

--- dice-game/src/components/ui/Box.jsx
+++ dice-game/src/components/ui/Box.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-function Box() {
-    return <div>Box</div>;
+function Box({ value = 1 }) {
+    return (
+        <>
+            <div
+                className="w-10 h-10 m-1 bg-slate-300 flex items-center justify-center"
+                onClick={() => setSelectNumber({ value })}
+            >
+                {value}
+            </div>
+        </>
+    );
 }
 
 export default Box;
--- dice-game/src/pages/Playground.jsx
+++ dice-game/src/pages/Playground.jsx
@@ -2,19 +2,26 @@ import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Box } from "@/components/ui";
 import { useId } from "react";
-import { useSearchParams } from "react-router-dom";
 function Playground() {
     const boxes = [];
     for (let i = 1; i <= 6; i++) {
         boxes.push(<Box key={useId()} value={i} />);
     }
+
     const [score, setScore] = useState(0);
     const [rules, setRules] = useState(false);
-    const [rule, setRule] = useState("show");
+    const [rule, setRule] = useState("Show");
+    const [selectNumber, setSelectNumber] = useState(6);
+
     const showRules = () => {
         setRules((prev) => !prev);
-        setRule((prev) => (prev == "show" ? "hide" : "show"));
+        setRule((prev) => (prev == "Show" ? "Hide" : "Show"));
     };
+
+    const handleReset = () => {
+        setScore(0);
+    };
+
     return (
         <>
             <div>
@@ -29,8 +36,10 @@ function Playground() {
             <div>
                 <div>Dice img</div>
                 <p>Click on dice to roll</p>
-                <Button variant="destructive">Reset score</Button>
-                <Button onClick={() => showRules()}>{rule} Rules</Button>
+                <Button variant="destructive" onClick={() => handleReset()}>
+                    Reset score
+                </Button>
+                <Button onClick={() => showRules()}>{rule} rules</Button>
             </div>
             {rules ? (
                 <div className="list-none">
