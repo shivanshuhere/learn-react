571c9ed1dd9b3679c260094c963947e8f0a1f11a
Shivanshu <ks7876555@gmail.com>
2024-01-22 16:58:30 +0530

    minor changes

--- dice-game/src/components/ui/Box.jsx
+++ dice-game/src/components/ui/Box.jsx
@@ -1,30 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Box({ value = 1 }) {
-    const [bg, setBg] = useState(false);
-    const handleClick = () => {
-        console.log("clicked", value);
-        setBg((prev) => !prev);
-    };
-    return (
-        <>
-            <div>
-                <label
-                    htmlFor=""
-                    className={` ${
-                        bg ? "bg-black" : "bg-white"
-                    } w-10 h-10 border-2 border-black text-center`}
-                >
-                    <input
-                        name="box"
-                        type="radio"
-                        onClick={() => handleClick()}
-                    ></input>{" "}
-                    {value}
-                </label>
-            </div>
-        </>
-    );
+function Box() {
+    return <div>Box</div>;
 }
 
 export default Box;
