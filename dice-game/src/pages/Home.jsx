c7666235dea9610c1707328623afc952a1d30c05
Shivanshu <ks7876555@gmail.com>
2024-01-23 20:58:33 +0530

    Add responsive style

--- dice-game/src/pages/Home.jsx
+++ dice-game/src/pages/Home.jsx
@@ -5,12 +5,27 @@ function Home() {
     const navigate = useNavigate();
     return (
         <>
-            <div>
-                <div>Img</div>
-                <div>
-                    <h1>Dice Game</h1>
+            <div
+                className="h-screen bg-black flex flex-col justify-center items-center
+            md:flex-row
+            "
+            >
+                <div className="w-3/4">
+                    <img
+                        src="https://images.pexels.com/photos/37534/cube-six-gambling-play-37534.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        alt=""
+                    />
+                </div>
+                <div className="w-1/2 flex  flex-col ">
+                    <h1
+                        className="font-mono text-2xl sm:text-3xl mb-4 text-slate-100
+                    md:text-5xl
+                    "
+                    >
+                        Dice Game
+                    </h1>
                     <Button
-                        className="mx-4 my-1"
+                        className=" text-slate-800  text-sm bg-slate-300 md:max-w-sm md:mr-8"
                         onClick={() => navigate("/playground")}
                     >
                         Play Now
