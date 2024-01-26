a9743a272c932dd7dc28b5f4df303e9f85d18783
Shivanshu <ks7876555@gmail.com>
2024-01-26 21:26:29 +0530

    style the card

--- foody-zone/src/components/Card.jsx
+++ foody-zone/src/components/Card.jsx
@@ -2,21 +2,21 @@ import { Badge } from "@/components/ui/badge";
 
 function Card() {
   return (
-    <div className="border-2 border-red-300 flex justify-center items-center w-11/12 bg-slate-500 backdrop-blur-sm rounded-md ">
-      <div className="w-1/2  grid place-items-center ">
+    <div className="border-2 border-red-300 flex justify-center items-center w-11/12 bg-slate-500 backdrop-blur-sm rounded-md gap-4">
+      <div className="w-1/3  grid place-items-center ">
         <img
-          className="rounded-full aspect-square"
+          className="rounded-full aspect-square w-full"
           src="https://www.cookwithmanali.com/wp-content/uploads/2017/06/Indian-Rice-Kheer.jpg"
           alt="img"
         />
       </div>
-      <div className="w-1/2  ">
+      <div className="w-1/2 p-2 ">
         <h2>Dish Name</h2>
         <p className="text-sm ">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi,
           dolorum?
         </p>
-        <Badge>price</Badge>
+        <Badge className="relative right-1">price</Badge>
       </div>
     </div>
   );
