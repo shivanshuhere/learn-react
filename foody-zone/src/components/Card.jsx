285314232415279ec2b044fcbe303e9c6bb02063
Shivanshu <ks7876555@gmail.com>
2024-01-27 20:35:20 +0530

    add responsiveness

--- foody-zone/src/components/Card.jsx
+++ foody-zone/src/components/Card.jsx
@@ -2,21 +2,28 @@ import { Badge } from "@/components/ui/badge";
 
 function Card() {
   return (
-    <div className="border-2 border-red-300 flex justify-center items-center w-11/12 bg-slate-500 backdrop-blur-sm rounded-md gap-4">
+    <div
+      className="
+      md:max-w-sm
+    sm:max-w-md
+    border-2 border-red-300 flex justify-center items-center w-5/6 bg-slate-500 backdrop-blur-sm rounded-md gap-4"
+    >
       <div className="w-1/3  grid place-items-center ">
         <img
-          className="rounded-full aspect-square w-full"
+          className="rounded-full aspect-square w-full p-2"
           src="https://www.cookwithmanali.com/wp-content/uploads/2017/06/Indian-Rice-Kheer.jpg"
           alt="img"
         />
       </div>
       <div className="w-1/2 p-2 ">
-        <h2>Dish Name</h2>
-        <p className="text-sm ">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi,
-          dolorum?
+        <h2 className="text-base mb-1">Dish Name</h2>
+        <p className="text-xs text-wrap mb-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+          voluptatem aperiam
         </p>
-        <Badge className="relative right-1">price</Badge>
+        <Badge variant="secondary" className="absolute right-2 bottom-2">
+          price
+        </Badge>
       </div>
     </div>
   );
--- foody-zone/src/components/Hero.jsx
+++ foody-zone/src/components/Hero.jsx
@@ -2,7 +2,12 @@ import { Card } from "../components";
 
 function Hero() {
   return (
-    <div className="min-h-screen flex flex-col  items-center gap-4 py-8 bg-[url('src/assets/foody-zone-bg.png')] bg-fixed">
+    <div
+      className="
+      md:flex-row md:flex-wrap md:justify-center
+      md:px-8
+    min-h-screen flex flex-col  items-center gap-4 md:gap-7 py-8 bg-[url('src/assets/foody-zone-bg.png')] bg-fixed bg-no-repeat bg-cover"
+    >
       <Card />
       <Card />
       <Card />
