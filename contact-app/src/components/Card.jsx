668f9dfb8fdcb441b8ca05af4722fa5b139df20a
Shivanshu <ks7876555@gmail.com>
2024-02-01 00:01:22 +0530

    add data from props

--- contact-app/src/components/Card.jsx
+++ contact-app/src/components/Card.jsx
@@ -1,17 +1,17 @@
-function Card() {
-  return (
-    <>
-      <div className="flex w-full m-2 rounded-lg bg-yellow-200 justify-center items-center ">
-        <div>avatar</div>
-        <div className="mx-1">
-          <h2>Name</h2>
-          <p>name123@gmail.com</p>
-        </div>
-        <button>Edit</button>
-        <button>Delete</button>
-      </div>
-    </>
-  );
+function Card({ name, email }) {
+    return (
+        <>
+            <div className="flex w-full m-2 rounded-lg bg-yellow-200 justify-evenly px-1 flex-wrap  items-center ">
+                <div>avatar</div>
+                <div className="mx-1">
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                </div>
+                <button>Edit</button>
+                <button>Delete</button>
+            </div>
+        </>
+    );
 }
 
 export default Card;
