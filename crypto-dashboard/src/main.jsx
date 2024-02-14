50d644899ee3bcd910d8b9509ff8cbbc11719c83
Shivanshu <ks7876555@gmail.com>
2024-02-14 20:28:14 +0530

    add routes and provider

--- crypto-dashboard/src/main.jsx
+++ crypto-dashboard/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Route, RouterProvider } from "react-router";
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+} from "react-router-dom";
+import { Dashboard, Transaction, Support } from "./pages/index.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />}>
+            <Route path="home" element={<Dashboard />} />
+            <Route path="transaction" element={<Transaction />} />
+            <Route path="support" element={<Support />} />
+        </Route>
+    )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router}>
+            <App />
+        </RouterProvider>
+    </React.StrictMode>
+);
