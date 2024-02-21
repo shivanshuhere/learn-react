737453143e20b15e1d1f770893562ca202d71ee2
Shivanshu <ks7876555@gmail.com>
2024-02-21 21:59:57 +0530

    minor changes

--- crypto-dashboard/src/App.jsx
+++ crypto-dashboard/src/App.jsx
@@ -2,12 +2,13 @@ import { Outlet } from "react-router";
 import Layout from "./layout/Layout";
 import SuccessVerify from "./components/SuccessVerify";
 import Forgot from "./components/Forgot";
+import SignUp from "./components/SignUp";
 
 function App() {
     return (
         <div className="flex justify-center items-center h-screen">
             {/* <Layout /> */}
-            <Forgot />
+            <SignUp />
         </div>
     );
 }
