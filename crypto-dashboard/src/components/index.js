815586913181fa220b2a0606d25fb4877387abf1
Shivanshu <ks7876555@gmail.com>
2024-02-29 22:39:27 +0530

    generalise :  comp imports

---
+++ crypto-dashboard/src/components/index.js
@@ -0,0 +1,19 @@
+import SignUp from "./SignUp";
+import Login from "./Login";
+import Forgot from "./Forgot";
+import EmailVerify from "./EmailVerify";
+import SuccessVerify from "./SuccessVerify";
+import PassResetDone from "./PassResetDone";
+import ResetPass from "./ResetPass";
+import SuccessSent from "./SuccessSent";
+
+export {
+    SignUp,
+    Login,
+    Forgot,
+    EmailVerify,
+    SuccessSent,
+    SuccessVerify,
+    PassResetDone,
+    ResetPass,
+};
