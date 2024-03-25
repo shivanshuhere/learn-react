a8dcb9d30592a31af6e65d3852d9a290cf55c444
Shivanshu <ks7876555@gmail.com>
2024-03-25 22:29:43 +0530

    add : styling

--- crypto-dashboard/src/components/ResetPass.jsx
+++ crypto-dashboard/src/components/ResetPass.jsx
@@ -1,18 +1,20 @@
 function ResetPass() {
     return (
         <>
-            <div>
-                <h2>Reset Password</h2>
-                <p>Enter your new password.</p>
-                <div>
-                    <label htmlFor="pass">New Password</label>
-                    <input id="pass" type="password" required />
-                </div>
-                <div>
-                    <label id="rePass" htmlFor="rePass">
-                        Repeat New Password
-                    </label>
-                    <input type="password" id="rePass" required />
+            <div className="flex bg-white flex-col h-screen justify-center items-center">
+                <div className="bg-slate-100 w-max px-10 py-20 flex flex-col gap-4 rounded-xl">
+                    <h2 className="sm:text-lg md:text-4xl">Reset Password</h2>
+                    <p className=" text-gray-500">Enter your new password.</p>
+                    <div className="flex w-full justify-between">
+                        <label htmlFor="pass">New Password</label>
+                        <input id="pass" type="password" required />
+                    </div>
+                    <div className="flex w-full justify-between">
+                        <label id="rePass" htmlFor="rePass">
+                            Repeat New Password
+                        </label>
+                        <input type="password" id="rePass" required />
+                    </div>
                 </div>
             </div>
         </>
